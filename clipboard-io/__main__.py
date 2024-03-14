9dfe332cd42d7ae6fe566e6dce4dc1c7e2c7f140
parker <parker.g.britt@gmail.com>
2024-03-14 13:11:07 +0000

    unpack nodeclip template to tmp dir

--- clipboard-io/__main__.py
+++ clipboard-io/__main__.py
@@ -1,4 +1,9 @@
 from .template import Template
+from .node_clip import NodeClip
 
-template = Template("/home/parker/Downloads/CPIO_Files/SOP_Pighead/SOP_copy.cpio")
-template.unpack(output="/home/parker/Downloads/CPIO_Files/SOP_Pighead/output")
+# template = Template("/home/parker/Downloads/CPIO_Files/SOP_Pighead/SOP_copy.cpio")
+# template.unpack(output="/home/parker/Downloads/CPIO_Files/SOP_Pighead/output", make_dirs=True)
+
+node_clip = NodeClip(template="/home/parker/Downloads/CPIO_Files/SOP_Pighead/SOP_copy.cpio")
+
+node_clip.unpack_template()
--- clipboard-io/node_clip.py
+++ clipboard-io/node_clip.py
@@ -1,12 +1,31 @@
+import tempfile, os, shutil
 from .template import Template
 
 class NodeClip():
-    def __init__(self, template: Template):
-        self.template = template
-        pass
+    def __init__(self, template: Template | str):
+        # get template object
+        if isinstance(template, str):
+            self.template = Template("template")
+        else:
+            self.template = template
+
+        self.is_unpacked=False
+        self.temp_dir=None
 
     def unpack_template(self):
-        self.template.checkout()
+        temp_dir = tempfile.mkdtemp(prefix="hclipboard-io_", suffix="_"+self.template.name)
+        print("New tmp file:", temp_dir)
+        self.temp_dir = temp_dir
+        # self.template.unpack(output=temp_dir)
+
+    def clear_tmp(self):
+        print("Removing tmp dir:", self.temp_dir)
+        if(self.temp_dir):
+            shutil.rmtree(self.temp_dir)
+
+    def __del__(self):
+        self.clear_tmp()
+        
 
 class Node():
     def __init__(self):
