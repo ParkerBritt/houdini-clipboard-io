d216f606322ed35a6c6bac50b9f2e057ee439d3f
parker <parker.g.britt@gmail.com>
2024-03-30 15:52:46 +0000

    partial implementation of parm optype parsing

--- hclipboard_io/parm.py
+++ hclipboard_io/parm.py
@@ -1,9 +1,46 @@
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Dict
+import re
 
 class Parm():
-    def __init__(self, name, args: list):
+    def __init__(self, name, properties: Dict[str, str]):
         self.name = name
-        self.args = args
+
+        self.init_properties(properties)
+
+    def init_properties(self, properties):
+        # label
+        label = properties.get('label', None)
+        if(label):
+            label = label.strip("\"")
+        self.label = label
+        # type
+        self.type = properties.get('type', None)
+        # size
+        size = properties.get('size', None)
+        if(size):
+            self.size: int = int(size)
+        self.size = size
+        # default
+        self.default = properties.get('default', None)
+        # range
+        parm_range = properties.get('range', None)
+        if(parm_range):
+            parm_range  = re.findall(r"-?\d+", parm_range)
+            parm_range = tuple(int(num) for num in parm_range)
+        self.range = parm_range
+        # parmtag
+        self.parmtag = properties.get('parmtag', None)
+
+        self.properties = [self.label, self.type, self.size, self.default, self.range, self.parmtag]
+
+        for prop in self.properties:
+            print("\nname", self.name)
+            print("label", self.label)
+            print("type", self.type)
+            print("size", self.size)
+            print("default", self.default)
+            print("range", self.range)
+            print("parmtag", self.parmtag)
 
     def set(self, value: Union[str, int, float, List[Union[str,int,float]]], index: Optional[int]=None):
         arg_len = len(self.args)
@@ -42,4 +79,4 @@ class Parm():
         return export
 
     def __str__(self):
-        return f"{self.name}:{self.args}"
+        return f"{self.name}:{self.properties}"
